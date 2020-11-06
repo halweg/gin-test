0b0a1aec85570dfc4d0e427ebf2cc9a231019e2c
halweg <halweg@126.com>
2020-11-07 00:27:04 +0800

    add param bind case

---
+++ param-bind/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+type Person struct {
+    Name string `form:"name"`
+    Age string `form:"age"`
+    Job string `form:"job"`
+}
+
+func main() {
+    r := gin.Default()
+    r.GET("/testing", testing)
+    r.Run()
+}
+
+func testing(c *gin.Context)  {
+    var person Person
+
+    if err := c.ShouldBind(&person); err != nil {
+        c.String(http.StatusOK, "%s", err.Error())
+    }
+
+    c.String(http.StatusOK, "%v", person)
+}
